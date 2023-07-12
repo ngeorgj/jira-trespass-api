6f5f4041e535b761aa57f23edccb58235c2ff311
Nivaldo Georg Junior <ngj.netrunner@gmail.com>
2023-07-12 20:55:32 -0300

    added the custom query endpoint

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import os
+
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -12,12 +13,12 @@ app = Flask(__name__)
 variables = os.environ
 
 # Connect to the database
-engine      = variables['DB_ENGINE']
-user        = variables['DB_USER']
-password    = variables['DB_PASSWORD']
-hostname    = variables['DB_HOSTNAME']
-port        = variables['DB_PORT']
-database    = variables['DB_DATABASE']
+engine = variables['DB_ENGINE']
+user = variables['DB_USER']
+password = variables['DB_PASSWORD']
+hostname = variables['DB_HOSTNAME']
+port = variables['DB_PORT']
+database = variables['DB_DATABASE']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{engine}://{user}:{password}@{hostname}:{port}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,6 +30,15 @@ db = SQLAlchemy(app)
 @app.route('/query/<table>', methods=['GET'])
 def query_table(table):
     result = db.session.execute(text(f"SELECT * FROM {table}"))
+    columns = result.keys()
+    rows = [dict(zip(columns, row)) for row in result]
+    return jsonify(rows)
+
+
+@app.route('/query', methods=['POST'])
+def query_table():
+    data = request.get_json()  # Retrieve JSON data from request
+    result = db.session.execute(text(data['query']))
     columns = result.keys()
     rows = [dict(zip(columns, row)) for row in result]
     return jsonify(rows)
